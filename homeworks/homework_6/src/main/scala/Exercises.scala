e7f024e9a78117ea67b1e0ac4268ae6debdebc35
Egor <Egor.Tushnolobov@urfu.me>
2020-12-15 15:43:58 +0500

    Task 1-4

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,27 +1,46 @@
+import Exercises.{morse, wordReverse}
+
 object Exercises {
 
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def fibonacci4Index(idx: Int): Int =
+    if (idx == 0)
+      0
+    else if (idx == 1 || idx == 2)
+      1
+    else
+      fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
 
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
-
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] =
+    for (i <- 0 to idx)
+      yield fibonacci4Index(i)
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String =
+    text.toUpperCase.split("").map(symbol =>
+      if (MORSE.contains(symbol))
+        MORSE(symbol)
+      else symbol).mkString(" ")
 
+  def wordReverse(text: String): String =
+    text
+      .split("(?=[!. ,?])|(?<=[!. ,?])")
+      .map(word => if (word.charAt(0).isUpper) {
+        word.toLowerCase.reverse.capitalize
+      } else
+        word.reverse
+      ).mkString("")
+}
 
-  def wordReverse(text: String): String = ???
-
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println(wordReverse("Зима!.. Крестьянин, торжествуя..."))
+    println(morse("Hello world!"))
+  }
 }
